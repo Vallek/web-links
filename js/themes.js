08f458db49ec9130d86524129acc25587d8502f4
Vallek <vforval86@gmail.com>
2024-01-07 19:40:03 +0300

    dark theme white flash fix

--- js/themes.js
+++ js/themes.js
@@ -4,14 +4,6 @@ const page = document.querySelector('.page');
 const lightThemeLink = document.querySelector('.themes__link_light');
 const darkThemeLink = document.querySelector('.themes__link_dark');
 
-// Check if user set dark theme before
-if (localStorage.getItem('weblinks-theme') == 'dark') {
-	page.classList.add('dark');
-}
-// Check if user prefers dark theme
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	page.classList.add('dark');
-}
 // Check theme preference switch real time
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
 	const newColorScheme = event.matches ? 'dark' : 'light';
