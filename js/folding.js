d1e28f43bdb478884e60894fbbe5dbac164f2780
Vallek <vforval86@gmail.com>
2024-02-28 11:28:24 +0300

    вариант свертывания

--- js/folding.js
+++ js/folding.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const foldContents = document.querySelector('.contents__fold');
+const foldContentsTitle = document.querySelector('.contents__fold__title');
+const foldedContentsTitle = document.querySelector('.contents__fold__title.contents__fold__title_show');
 const foldContentsInput = document.querySelector('#fold-contents');
 const topics = document.querySelectorAll('.contents-list__link');
 const topicsList = document.querySelectorAll('.contents-list__topics');
@@ -9,8 +11,13 @@ foldContentsInput.addEventListener('click', foldCOntents);
 
 function foldCOntents() {
 	topicsList.forEach(el => {
+		// Hide lists
 		el.classList.toggle('visually-hidden');
-		foldContents.classList.toggle('contents__fold_fold');
+		// Eye icon change
+		foldContents.classList.toggle('contents__fold_folded');
+		foldContentsTitle.classList.toggle('contents__fold__title_show');
+		foldedContentsTitle.classList.toggle('contents__fold__title_show');
+		// Contents arrows
 		topics.forEach(topic => {
 			topic.classList.toggle('contents-list__link_folded');
 		});
