588ab8bc4cae12445829df1adb6ae9413ddadc83
Vallek <vforval86@gmail.com>
2024-02-29 19:42:09 +0300

    анимация содержания

--- js/folding.js
+++ js/folding.js
@@ -8,10 +8,10 @@ const topicsList = document.querySelectorAll('.contents__topics');
 
 foldInput.addEventListener('click', foldCOntents);
 foldButton.forEach(el => {
-	el.addEventListener('click', foldList);
+	el.addEventListener('click', foldListAll);
 });
 
-// Toggle all button
+// Toggle-all button
 function foldCOntents(event) {
 	// Change title
 	const unfoldTitle = document.querySelector('.fold__title_show');
@@ -19,9 +19,9 @@ function foldCOntents(event) {
 	unfoldTitle.classList.toggle('visually-hidden');
 	foldTitle.classList.toggle('visually-hidden');
 	// Fold and unfold lists
-	if (foldInput.value == 'show') {
+	if (foldInput.value === 'show') {
 		topicsList.forEach(el => {
-			el.classList.add('visually-hidden');
+			foldList(el);
 			foldInput.value = 'hide';
 		});
 			// Contents arrows
@@ -29,9 +29,9 @@ function foldCOntents(event) {
 			topic.classList.add('list__fold-button_folded');
 		});
 	} 
-	else if (foldInput.value == 'hide') {
+	else if (foldInput.value === 'hide') {
 		topicsList.forEach(el => {
-			el.classList.remove('visually-hidden');
+			foldList(el);
 			foldInput.value = 'show';
 		});
 			// Contents arrows
@@ -44,10 +44,26 @@ function foldCOntents(event) {
 }
 
 // Fold/unfold each
-function foldList(el) {
+function foldListAll(el) {
 	let thisItem = el.target.parentNode;
 	let thisLIst = thisItem.parentNode.querySelector('.contents__topics');
 	let thisButton = thisItem.querySelector('.list__fold-button');
-	thisLIst.classList.toggle('visually-hidden');
 	thisButton.classList.toggle('list__fold-button_folded');
+	foldList(thisLIst);
+}
+
+// Fold list for css transition with height detection
+function foldList(el) {
+	if (el.style.height === "0px") {
+		el.style.height = el.scrollHeight + 'px';
+	} else {
+		el.style.height = el.scrollHeight + 'px';
+			window.getComputedStyle(el, null).getPropertyValue("height");
+			el.style.height = "0";
+	}
+	el.addEventListener("transitionend", () => {
+		if (el.style.height !== "0px") {
+				el.style.height = "auto";
+		}
+	});
 }
