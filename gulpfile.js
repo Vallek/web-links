d7324effd4a7617634bb2143fc000d6254c5d1bb
Vallek <vforval86@gmail.com>
2024-03-23 15:38:15 +0300

    gulp для сборки js

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp'); 
+const concat = require('gulp-concat');
+const del = require('del');
+const header = require('gulp-header');
+
+gulp.task('scripts', function() {
+  let clean = del("./js/main.js");
+	let scripts = gulp.src(['./js/themes.js', './js/header.js', './js/folding.js'])
+		.pipe(concat('main.js', {newLine: '\n\n'}))
+		.pipe(header("'use strict';\n"))
+		.pipe(gulp.dest('./js/'));
+
+	return Promise.all([clean, scripts]);
+});
