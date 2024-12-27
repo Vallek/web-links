beb7dd7c8ae3536e7ae1845170bf0296476020b6
Vallek <vforval86@gmail.com>
2024-12-27 17:45:33 +0300

    рефакторинг gulp сборки на переменные + watch сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
-const gulp = require('gulp'); 
+const { src, dest, watch } = require('gulp');
 const concat = require('gulp-concat');
-const del = require('del');
 const header = require('gulp-header');
 
-gulp.task('scripts', function() {
-  let clean = del("./js/main.js");
-	let scripts = gulp.src(['./src/js/themes.js', './src/js/header.js', './src/js/folding.js', './src/js/page-search.js'])
-		.pipe(concat('main.js', {newLine: '\n\n'}))
-		// .pipe(header("const page = document.querySelector('.page');\n"))
-		.pipe(header("'use strict';\n\n"))
-		.pipe(gulp.dest('./js/'));
+const styles = () => {
+	return src(['./src/css/style.css', './src/css/emojis.css', './src/css/favicons.css'])
+	.pipe(concat('main.css', {newLine: '\n\n'}))
+	.pipe(dest('./css/'));
+}
 
-	return Promise.all([clean, scripts]);
-});
+const stylesEn = () => {
+	return src(['./src/css/style.css', './src/css/emojis.css', './src/css/favicons_EN.css'])
+	.pipe(concat('main_EN.css', {newLine: '\n\n'}))
+	.pipe(dest('./css/'));
+}
 
-gulp.task('styles', function() {
-  let clean = del("./css/main.css");
-	let styles = gulp.src(['./src/css/style.css', './src/css/emojis.css', './src/css/favicons.css'])
-		.pipe(concat('main.css', {newLine: '\n\n'}))
-		.pipe(gulp.dest('./css/'));
+const scripts = () => {
+	return src(['./src/js/themes.js', './src/js/header.js', './src/js/folding.js', './src/js/page-search.js'])
+	.pipe(concat('main.js', {newLine: '\n\n'}))
+	.pipe(header("'use strict';\n\n"))
+	.pipe(dest('./js/'));
+}
 
-	return Promise.all([clean, styles]);
-});
+const watchDev = () => {
+	watch('./src/css/*.css', styles);
+	watch('./src/css/*.css', stylesEn);
+	watch('./src/js/*.js', scripts);
+}
 
-gulp.task('stylesEN', function() {
-  let clean = del("./css/mainEN.css");
-	let stylesEN = gulp.src(['./src/css/style.css', './src/css/emojis.css', './src/css/favicons_EN.css'])
-		.pipe(concat('main_EN.css', {newLine: '\n\n'}))
-		.pipe(gulp.dest('./css/'));	
-
-	return Promise.all([clean, stylesEN]);
-});
+exports.styles = styles;
+exports.stylesEn = stylesEn;
+exports.scripts = scripts;
+exports.watchDev = watchDev;
